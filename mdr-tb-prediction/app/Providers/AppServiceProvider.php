948e1ee3ff585ec65c6085e515db28abdfb69db5
radacore <rahmatdanisyw@gmail.com>
2026-04-06 22:27:10 +0800

    konfig

--- mdr-tb-prediction/app/Providers/AppServiceProvider.php
+++ mdr-tb-prediction/app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Support\Facades\Vite;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\URL; // <--- Tambahkan import ini
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -20,6 +21,11 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        // Tambahkan baris ini untuk memaksa HTTPS di server produksi
+        if (config('app.env') === 'production') {
+            URL::forceScheme('https');
+        }
+
         Vite::prefetch(concurrency: 3);
     }
 }
